e3b62df0c62266717614d175ddc2a28ad45abf1a
Abinaya <abinaya.arumugam@petrustechnologies.com>
2024-02-12 17:06:53 +0530

    user table header changed

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { UserTableHeader } from '../constants/User';
 
 function createData(
   name: string,
@@ -31,11 +32,14 @@ export default function UserList() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
+            {UserTableHeader.map((item, index) => (
+              <TableCell key={index+1}>{item.label}</TableCell>
+            ))}
+
+            {/* <TableCell align="right">Calories</TableCell>
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
---
+++ src/constants/User.tsx
@@ -0,0 +1,8 @@
+export const UserTableHeader = [
+    {label:"Name",name:"name"},
+    {label:"User ID", name:"userid"},
+    {label:"Email", name:"email"},
+    {label:"Mobile Number", name:"mobile_number"},
+    {label:"Department", name:"department"},
+    {label:"Role", name:"role"}
+]
