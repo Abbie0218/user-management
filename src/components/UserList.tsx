207e5d07013ab0f2bd60daee23ab9b052ad2aab3
Abinaya <abinaya.arumugam@petrustechnologies.com>
2024-02-13 08:03:13 +0530

    added image

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -7,23 +7,19 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { UserData, UserTableHeader } from '../constants/User';
-import Checkbox from '@mui/material/Checkbox';
+import { Avatar, Stack } from '@mui/material';
 
 export default function UserList() {
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} style={{marginTop:"28px"}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell></TableCell>
             {UserTableHeader.map((item, index) => (
               <TableCell key={index + 1}>{item.label}</TableCell>
             ))}
             <TableCell></TableCell>
-            {/* <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+            <TableCell></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -32,15 +28,22 @@ export default function UserList() {
               key={item.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell> <Checkbox /></TableCell>
-              <TableCell component="th" scope="row">
-                {item.name}
+               <TableCell>
+                <Stack direction='row' spacing={2}>
+                  <Avatar
+                    alt={item.name}
+                    src="https://media.istockphoto.com/id/954956446/photo/graduation-celebration.jpg?s=1024x1024&w=is&k=20&c=dqqRmkBua9C7AlFxUg3XTCrR5W1hG1lfuY20IY6iIr0="
+                    sx={{ width: 24, height: 24, borderRadius: "4px" }}
+                    variant='square'
+                  />
+                  <span> {item.name}</span>
+                </Stack>
               </TableCell>
-              <TableCell align="right">{item.userId}</TableCell>
-              <TableCell align="right">{item.email}</TableCell>
-              <TableCell align="right">{item.mobile_number}</TableCell>
-              <TableCell align="right">{item.department}</TableCell>
-              <TableCell align="right">{item.role}</TableCell>
+              <TableCell>{item.userId}</TableCell>
+              <TableCell>{item.email}</TableCell>
+              <TableCell>{item.mobile_number}</TableCell>
+              <TableCell>{item.department}</TableCell>
+              <TableCell>{item.role}</TableCell>
               <TableCell></TableCell>
             </TableRow>
           ))}
