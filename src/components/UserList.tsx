e2dc84836f00fb0eef8f0ce24bf195160087e5e1
Abinaya <abinaya.arumugam@petrustechnologies.com>
2024-02-13 08:15:39 +0530

    action included

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -7,19 +7,35 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { UserData, UserTableHeader } from '../constants/User';
-import { Avatar, Stack } from '@mui/material';
+import { Avatar, IconButton, Stack } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function UserList() {
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   return (
     <TableContainer component={Paper} style={{marginTop:"28px"}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             {UserTableHeader.map((item, index) => (
-              <TableCell key={index + 1}>{item.label}</TableCell>
+              <TableCell key={index + 1} sx={{fontWeight:700}}>{item.label}</TableCell>
             ))}
-            <TableCell></TableCell>
-            <TableCell></TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell sx={{visibility:"hidden"}}>Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -44,11 +60,26 @@ export default function UserList() {
               <TableCell>{item.mobile_number}</TableCell>
               <TableCell>{item.department}</TableCell>
               <TableCell>{item.role}</TableCell>
-              <TableCell></TableCell>
+              <TableCell><IconButton onClick={handleClick}><MoreHorizIcon /></IconButton></TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+
     </TableContainer>
   );
 }
